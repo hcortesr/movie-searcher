2fb1c0aff46ff2c0c4329e20349170b88b4e040c
Soy Hector <heccorram@gmail.com>
2025-02-08 13:51:43 -0600

    Ya tiene todo2

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const express = require('express');
 const { request } = require('http');
 const app = express();
 const pg = require('pg')
+const ejs = require('ejs');
+
+app.set('views', './res/views');
+app.use(express.static('./res'));
 
 app.get('/', (req, res) => {
     res.sendFile('./res/index.html', {
@@ -14,20 +18,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/search', (req, res) => {
-    const { search } = req.query;
+    const { s } = req.query;
     const url = new URL('http://www.omdbapi.com/?apikey=b939813f&');
-    url.searchParams.append('s', search);
+    url.searchParams.append('s', s);
     fetch(url, { method: 'get' })
         .then(res => res.json())
         .then(data => {
-            console.log(data)
-            res.json(data);
+            res.render('index.ejs', { results: data['Search'] })
         })
         .catch(error => {
             res.send("Hubo un error", error);
         })
 
 });
+
+app.get('/render', (req, res) => {
+    res.render("adf");
+})
 
 app.get('/get', (req, res) => {
     const { search } = req.query;
